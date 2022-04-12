6d423d5cfb80fecd30b3946b3bf4a059a6609fe8
anwarkhan0 <anwarsyed01234@gmail.com>
2022-04-12 06:34:34 +0500

    main part 3 completed

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import delivery from '../img/delivery.png'
 import HeroBg from '../img/heroBg.png'
+import iceImg from '../img/i1.png'
+import imgOne from '../img/f1.png'
+import imgTwo from '../img/cu1.png'
+import imgThree from '../img/c6.png'
+
 export default function HomeContainer() {
   return (
     <section className='grid grid-cols-1 md:grid-cols-2 gap-2 w-full mt-5' id='home'>
@@ -11,18 +16,41 @@ export default function HomeContainer() {
             <img src={delivery} className='w-full h-full bg-white rounded-full object-contain' alt="" />
           </div>
         </div>
-        <p className='text-[3.5rem] lg:text-[4rem] font-bold tracking-wide text'>
-          The Fastest Delivery in <span className='text-orange-600 text-[4.5rem] lg:text-[5rem]'>Your City</span>
+        <p className='text-[3.5rem] lg:text-[4rem] font-bold tracking-wide'>
+          The Best Food provider in <span className='text-orange-600 text-[4.5rem] lg:text-[5rem]'>Your Town</span>
         </p>
         <p className='text-base text-center md:text-left'>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
 
         <button type='button' className='bg-gradient-to-br md:w-auto from-orange-500 to-orange-600 w-full py-2 rounded-lg hover:shadow-lg transition-all ease-in duration-100 p-3'>Order Now</button>
       </div>
 
-      <div className='py-2 flex-1 flex items-center '>
+      <div className='py-2 flex-1 flex md:items-center'>
         <img src={HeroBg} className='h-96 w-full md:w-auto ml-auto md:h-auto' alt="" />
-        <div className='absolute flex items-center justify-center'>
-            <div className='w-190 p-2 bg-red-500'></div>
+        <div className='absolute flex flex-wrap mt-12 justify-center items-center gap-12 pt-6'>
+            <div className=' md:w-[190px] h-32 basis-2/5 md:basis-auto md:h-52 p-2 bg-purple-100 backdrop-blur-md rounded-3xl flex flex-col items-center justify-center'>
+                <img src={iceImg} className='w-28 md:w-40 -mt-20' alt="" />
+                <p className='text-base font-semibold'>Ice Cream</p>
+                <p className='text-sm text-gray-400'>vanilla and something else</p>
+                <p className='text-sm font-semibold'>2.5 <span className='text-red-500'>$</span></p>
+            </div>
+            <div className=' md:w-[190px] h-32 basis-2/5 md:basis-auto md:h-52 p-2 bg-purple-100 backdrop-blur-md rounded-3xl flex flex-col items-center justify-center'>
+                <img src={imgOne} className='w-28 md:w-40 -mt-20' alt="" />
+                <p className='text-base font-semibold'>Straberry</p>
+                <p className='text-sm text-gray-400'>vanilla and something else</p>
+                <p className='text-sm font-semibold'>2.5 <span className='text-red-500'>$</span></p>
+            </div>
+            <div className=' md:w-[190px] h-32 basis-2/5 md:basis-auto md:h-52 p-2 bg-purple-100 backdrop-blur-md rounded-3xl flex flex-col items-center justify-center'>
+                <img src={imgTwo} className='w-28 md:w-40 -mt-20' alt="" />
+                <p className='text-base font-semibold'>Chicken Karahi</p>
+                <p className='text-sm text-gray-400'>vanilla and something else</p>
+                <p className='text-sm font-semibold'>2.5 <span className='text-red-500'>$</span></p>
+            </div>
+            <div className=' md:w-[190px] h-32 basis-2/5 md:basis-auto md:h-52 p-2 bg-purple-100 backdrop-blur-md rounded-3xl flex flex-col items-center justify-center'>
+                <img src={imgThree} className='w-28 md:w-40 -mt-20' alt="" />
+                <p className='text-base font-semibold'>Fried Fish</p>
+                <p className='text-sm text-gray-400'>vanilla and something else</p>
+                <p className='text-sm font-semibold'>2.5 <span className='text-red-500'>$</span></p>
+            </div>
         </div>
       </div>
       
